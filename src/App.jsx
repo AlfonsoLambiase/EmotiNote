27eaf012f0e898994082cc8ab004cf909cab26ea
Alfonso Lambiase <lambiase1199@gmail.com>
2025-05-14 21:15:49 +0200

    add Home

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import Diario from "./pages/Diario";
 import Today from "./pages/Today";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Mp3 from "./components/Mp3";
 import { createContext, useEffect, useState } from "react";
+
 
 export const DiarioContext = createContext();
 
@@ -27,11 +29,12 @@ function App() {
     localStorage.setItem("diario", JSON.stringify(diario));
   }, [diario]);
   console.log(diario);
-  
+
   return (
     <>
       <DiarioContext.Provider value={diario}>
-        <Header />
+        <Header/>
+        <Mp3/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/diario" element={<Diario />} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,26 @@
-const Home=()=>{
 
-    return(
-        <>
-        
-        <p>Home</p>
-        </>
-    )
-}
-export default Home
+
+const Home = () => {
+  return (
+    <>
+      <div className="flex justify-center">
+        <p>
+          Benvenuto nel tuo diario personale.
+          <br />
+          Questo è il tuo spazio sicuro, dove ogni emozione è accolta con rispetto.
+          <br />
+          Qui puoi monitorare il tuo stato d’animo giorno dopo giorno e coltivare il tuo benessere emotivo.
+          <br />
+          Esprimere ciò che provi è il primo passo per prenderti cura di te.
+          <br />
+          Aiuta la tua salute mentale, ascoltati, scriviti, liberati. Sentiti libero di essere te stesso, senza filtri né giudizi.
+          <br />
+          Le tue emozioni contano.
+        </p>
+    
+      </div>
+    </>
+  );
+};
+
+export default Home;
