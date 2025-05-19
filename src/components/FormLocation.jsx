9dd2882f975794105cfaeed9b1ef2e33217212c7
natasciarufino <natasciarufino17@gmail.com>
2025-05-19 15:33:45 +0200

    fix page FormLocation

--- src/components/FormLocation.jsx
+++ src/components/FormLocation.jsx
@@ -52,7 +52,7 @@ const FormLocation = () => {
   };
 
   return (
-    <div className="flex items-start justify-center min-h-screen gap-6 px-4 pt-12">
+    <div className="flex items-start justify-center gap-6 px-4 pt-12">
       {/* Animazioni lato sinistro */}
       <div className="flex flex-col gap-6">
         <Lottie
